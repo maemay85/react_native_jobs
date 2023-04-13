6e6d4ffc38f4c1710b590a6574a4bfdebbefa4b9
maemay85 <hellomaemay@gmail.com>
2023-04-13 16:58:16 -0400

    search button added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,8 +1,10 @@
+
 import React from 'react'
 import { useState } from 'react'
 import {
   View,
   Text,
+  Image,
   TextInput,
   TouchableOpacity,
   FlatList
@@ -22,8 +24,20 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={()=> {}}
+            placeholder='What are you looking for?'
+          />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={styles.searchBtnImage}
+            />
+          </TouchableOpacity>
       </View>
 
     </View>
